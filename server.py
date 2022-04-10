fca483d394ae03c65ab22edda21f1c6969a33133
Artem Mozhegov <amozhegov@yahoo.com>
2022-04-11 02:58:40 +0400

    Update server.py
    added broadcast and handle functions to work with client messages

--- server.py
+++ server.py
@@ -3,3 +3,26 @@ import socket
 
 host = '192.168.100.4'
 port = 9009
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+clients_names = []
+
+def broadcast(message):
+	for client in clients:
+		client.send(message)
+
+def handle(client):
+	while True:
+		try:
+			message = client.recv(1024)
+			broadcast(message)
+		except:
+			index = clients.index(client)
+			
+
+
+
